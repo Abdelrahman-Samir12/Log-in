0dd21f2ccf7a311f4e108c3c4de2a97ac1019962
Abdelrahman_Samir <abdelrahmansamir326@gmail.com>
2022-10-17 21:09:39 +0200

    First

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Get /about
+Rails.application.routes.draw do
+
+  get "about-us", to: "about#index", as: :about
+  
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  delete "logout", to: "sessions#destroy"
+  
+  root to: "main#index"
+end
